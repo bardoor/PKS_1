0b018915b4be79f1fed6c74b580951ed23bfb974
shadowflexer <ikhanin2008@mail.ru>
2024-02-08 12:34:30 +0300

    Создал методы для поиска примерной площади и периметра круга
    В методах для отсутствия магических чисел объявлена переменная π = 3.14, т.к. значение округлено, в обоих функциях результат не точный, метод find_S - находит площадь круга по формуле S = π*r**2 и возвращает значение, метод find_P - находит периметр круга по формуле P = 2*π*r и возвращает значение

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ class Rectangle:
 class Circle:
     def __init__(self, radius_length: float) -> None:
         self.radius_length = radius_length
+
+    def find_S(self) -> float:
+        π = 3.14
+        return π * (self.radius_length ** 2)
+    
+    def find_P(self) -> float:
+        π = 3.14
+        return (2 * π * self.radius_length)
