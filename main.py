9d9d67a7633300bbcefd57eb7b90a8833e978883
shadowflexer <ikhanin2008@mail.ru>
2024-02-08 12:22:32 +0300

    Создал методы по поиску площади и периметра у класса прямоугольник
    find_S - метод возвращающий площадь прямоугольника путем перемножения его 2 смежных сторон, find_P - метод возвращающий периметр прямоугольника путем сложения его 2 смежных сторон и умножением этой суммы на 2

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ class Rectangle:
     def __init__(self, first_side_length: float, second_side_length: float) -> None:
         self.first_side_length = first_side_length
         self.second_side_length = second_side_length
+
+    def find_S(self) -> float:
+        return (self.first_side_length * self.second_side_length)
+    
+    def find_P(self) -> float:
+        return (self.first_side_length + self.second_side_length) * 2
