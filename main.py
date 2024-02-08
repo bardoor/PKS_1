6fc6a62ebb8bfcdd0f5d18983be67124261e2138
shadowflexer <ikhanin2008@mail.ru>
2024-02-08 12:17:13 +0300

    Создал класс rectangle(прямоугольник) и метод __init__
    Класс прямоугольник имеет 2 свойства: 2 стороны - first_side_length и second_side_length(стороны смежные), обе задаются в __init__ в типе данных float

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ class Square:
     
     def find_P(self) -> float:
         return (self.side_length * 4)
+    
+
+class Rectangle:
+    def __init__(self, first_side_length: float, second_side_length: float) -> None:
+        self.first_side_length = first_side_length
+        self.second_side_length = second_side_length
