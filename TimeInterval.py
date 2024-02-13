f589908501d127acfaa1356b52977d6578bf6de0
shadowflexer <ikhanin2008@mail.ru>
2024-02-13 15:51:35 +0300

    Создал методы __add__ и __sub__ для класса TimeInterval
    Оба метода имеют 2 параметра: self, other - Эти параметры должны являться объектами класса TimeInterval; __add__ - Возвращает объект класса TimeInterval с значением поля seconds равного сумме полей seconds переданных в метод объектов(self.seconds + other.seconds); __sub__ - Возвращает объект класса TimeInterval с значением поля seconds равного разница полей seconds переданных в метод объектов(self.seconds - other.seconds)

--- TimeInterval.py
+++ TimeInterval.py
@@ -3,3 +3,9 @@ class TimeInterval:
 
     def __init__(self, seconds: int, minutes: int = 0, hours: int = 0) -> None:
         self.seconds = seconds + minutes * 60 + hours * 3600
+
+    def __add__(self, other):
+        return TimeInterval(self.seconds + other.seconds)
+
+    def __sub__(self, other):
+        return TimeInterval(self.seconds - other.seconds)
