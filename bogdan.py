b2644f9c075f9a69a335a915263fe7cf8e8e04b6
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-02-13 23:02:33 +0300

    Добавлен класс Rectangle
    Добавлен класс Rectangle, а так-же методы по нахождению Площади и Периметра.

--- bogdan.py
+++ bogdan.py
@@ -9,3 +9,15 @@ class Square: # Квадрат
 
     def find_perimeter(self):
         return self.side_length * 4
+
+
+class Rectangle: # Прямоугольник
+    def __init__(self, height, width):
+        self.width = width
+        self.height = height
+
+    def find_square(self):
+        return self.width * self.height
+
+    def find_perimeter(self):
+        return self.width * 2 + self.height * 2
