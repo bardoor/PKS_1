ef02904138da8fef1fdc433ae0f968b650376924
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-02-13 23:00:08 +0300

    Добавлен класс Square
    Добавлен класс Square, а так-же методы по нахождению Площади и Периметра.

--- bogdan.py
+++ bogdan.py
@@ -1,1 +1,11 @@
 import math
+
+class Square: # Квадрат
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def find_square(self):
+        return self.side_length ** 2
+
+    def find_perimeter(self):
+        return self.side_length * 4
