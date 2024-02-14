2c6daada4c741952978c073be56a896145960ac1
shadowflexer <ikhanin2008@mail.ru>
2024-02-14 18:14:36 +0300

    Создал метод get_side_length для класса Square
    Метод принимает 1 параметр - self; Предположительно возвращает объект типа float, т.к. возвращает self.side_length (Метод добавлен перед остальными и после метода __init__)

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ class Square:
     def __init__(self, side_length: float) -> None:
         self.side_length = side_length
 
+    def get_side_length(self) -> float:
+        return self.side_length
+
     def find_area(self) -> float:
         return (self.side_length ** 2)
     
     def find_perimeter(self) -> float:
         return (self.side_length * 4)
-    
 
 class Rectangle:
     def __init__(self, first_side_length: float, second_side_length: float) -> None:
