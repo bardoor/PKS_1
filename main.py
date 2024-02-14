45afd94585b81e4b0d9b2edf19570857bd074415
shadowflexer <ikhanin2008@mail.ru>
2024-02-14 18:20:56 +0300

    Создал 2 метода получения сторон у класса Rectangle
    Названия методов: get_first_side_length, get_second_side_length; Оба метода принимают 1 параметр - self; Оба метода предположительно возвращают объект типа float, т.к. get_first_side_length - возвращает self.first_side_length, а get_second_side_length - возвращает self.second_side_length. Оба метода расположены до методов по поиску площади и периметра и после метода __init__

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ class Square:
     def find_perimeter(self) -> float:
         return (self.side_length * 4)
 
+
 class Rectangle:
     def __init__(self, first_side_length: float, second_side_length: float) -> None:
         self.first_side_length = first_side_length
         self.second_side_length = second_side_length
+
+    def get_first_side_length(self) -> float:
+        return self.first_side_length
+    
+    def get_second_side_length(self) -> float:
+        return self.second_side_length
 
     def find_area(self) -> float:
         return (self.first_side_length * self.second_side_length)
     
     def find_perimeter(self) -> float:
         return (self.first_side_length + self.second_side_length) * 2
-    
+
 
 class Circle:
     def __init__(self, radius_length: float) -> None:
