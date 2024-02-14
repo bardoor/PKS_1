23cf275b3d8ecbce7a161ae4c5ad55db9796c864
Tumani1337 <idi_suda06@mail.ru>
2024-02-14 09:56:28 +0300

    создал класс Timeinterval

---
+++ Timeinterval.py
@@ -0,0 +1,18 @@
+class Timeinterval:
+    seconds: int
+    def __init__(self, seconds, minuts: int=0, hours: int=0):
+        self.seconds = seconds + minuts * 60 + hours * 3600
+
+    def __add__(self,other):
+        return Timeinterval(self.seconds + other.seconds)
+
+    def __sub__(self, other):
+        return Timeinterval(self.seconds - other.seconds)
+
+    def __str__(self) -> str:
+        hours = self.seconds // 3600
+        minutes = (self.seconds - hours * 3600) // 60
+        seconds = self.seconds - hours * 3600 - minutes * 60
+        return f"Временной интервал содержит {hours} часов, {minutes} минут и {seconds} секунд"
+
+
