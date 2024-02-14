b8ddd6700a82c23beb642ce08f53e0e0299d0fac
shadowflexer <ikhanin2008@mail.ru>
2024-02-14 17:05:39 +0300

    Повысил производительность кода и повысил его читабельность
    Все далее описаное находится в классе TimeInterval: У метода __init__ теперь для параметра seconds по умолчанию стоит значение 0, тоесть seconds: int = 0; Теперь для всех методов сравнения объектов класса убраны ненужные условия(сокращение кода); Метод __str__ перенесен в самый конец методов(теперь он последний)

--- TimeInterval.py
+++ TimeInterval.py
@@ -1,7 +1,7 @@
 class TimeInterval:
     seconds: int
 
-    def __init__(self, seconds: int, minutes: int = 0, hours: int = 0) -> None:
+    def __init__(self, seconds: int=0, minutes: int=0, hours: int=0) -> None:
         self.seconds = seconds + minutes * 60 + hours * 3600
 
     def __add__(self, other):
@@ -10,41 +10,23 @@ class TimeInterval:
     def __sub__(self, other):
         return TimeInterval(self.seconds - other.seconds)
     
-    def __str__(self) -> str:
-        hours = self.seconds // 3600
-        minutes = (self.seconds - hours * 3600) // 60
-        seconds = self.seconds - hours * 3600 - minutes * 60
-        return f"В данном промежутке времени есть {hours} часов, {minutes} минут, {seconds} секунд"
-    
     def __eq__(self, other) -> bool:
-        if self.seconds == other.seconds:
-            return True
-        return False
+        return self.seconds == other.seconds
     
     def __ne__(self, other) -> bool:
-        if self.seconds != other.seconds:
-            return True
-        return False
+        return self.seconds != other.seconds
     
     def __lt__(self, other) -> bool:
-        if self.seconds < other.seconds:
-            return True
-        return False
+        return self.seconds < other.seconds
 
     def __gt__(self, other) -> bool:
-        if self.seconds > other.seconds:
-            return True
-        return False
+        return self.seconds > other.seconds
     
     def __le__(self, other) -> bool:
-        if self.seconds <= other.seconds:
-            return True
-        return False
+        return self.seconds <= other.seconds
 
     def __ge__(self, other) -> bool:
-        if self.seconds >= other.seconds:
-            return True
-        return False
+        return self.seconds >= other.seconds
     
     def get_time_interval_in_nanoseconds(self) -> int:
         return self.seconds * 1000000000
@@ -60,3 +42,9 @@ class TimeInterval:
     
     def get_time_interval_in_days(self) -> float:
         return self.seconds / 86400
+    
+    def __str__(self) -> str:
+        hours = self.seconds // 3600
+        minutes = (self.seconds - hours * 3600) // 60
+        seconds = self.seconds - hours * 3600 - minutes * 60
+        return f"В данном промежутке времени есть {hours} часов, {minutes} минут, {seconds} секунд"
