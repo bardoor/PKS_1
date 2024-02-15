d14b798d54fb379d035c2601d8397aa1f2b81652
Maga <mcmagakrytoy@gmail.com>
2024-02-15 22:11:54 +0300

    создание функции get_in_seconds
    для перевода других единиц измерений в секунды

--- work_time.py
+++ work_time.py
@@ -4,3 +4,6 @@ class TimeInterval:
     def __init__(self,seconds: int,minuts: int=0,hours: int=0) -> None:
         self.seconds = seconds + minuts * 60 + hours * 3600
 
+    def get_in_seconds(self):
+        result = self.seconds
+        return result
