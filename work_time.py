bde810d9711254d375d93d43f198208e93adc485
Maga <mcmagakrytoy@gmail.com>
2024-02-15 22:13:47 +0300

    добавление __str__
    для вывода времени(часы,минуты,секунды)

--- work_time.py
+++ work_time.py
@@ -4,6 +4,13 @@ class TimeInterval:
     def __init__(self,seconds: int,minuts: int=0,hours: int=0) -> None:
         self.seconds = seconds + minuts * 60 + hours * 3600
 
-    def get_in_seconds(self):
+    def get_in_seconds(self) -> int:
         result = self.seconds
         return result
+    
+    def __str__(self) -> str:
+        hours = self.seconds // 3600
+        minuts = (self.seconds - hours * 3600) // 60
+        seconds = self.seconds - hours * 3600 - minuts * 60
+
+        return f"{hours}:{minuts}:{seconds}"
