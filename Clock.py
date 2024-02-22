ee8d91d5d15216294511b084903c67b2c6f88fdc
shadowflexer <ikhanin2008@mail.ru>
2024-02-22 11:32:25 +0300

    Создал метод __init__ для класса Clock
    Этот метод принимает 3 параметра: self, hours: int=0, minutes: int=0; Последние 2 параметра имеют значение по умолчанию: 0, а также предположительно должны принимать объкт типа данных: int; Метод возвращает None(то есть ничего не возвращает); Перед тем как установить значение для полей в классе, значения параметров проходят проверки: если не(0 <= hours <= 23) - то для hours устанавливается значение 0, затем вне условия устанавливается self.__hours = hours; Следуящая проверка: если не(0 <= minutes <= 59) - то для minutes устанавливается значение 0, затем вне условия self.__minutes = minutes. Данный метод допускает ввод некорректного пользователем числа и в случае этого для неправильно введенного значения устанавливает значение 0

--- Clock.py
+++ Clock.py
@@ -1,3 +1,11 @@
 class Clock:
     __hours: int
     __minutes: int
+
+    def __init__(self, hours: int=0, minutes: int=0) -> None:
+        if not(0 <= hours <= 23):
+            hours = 0
+        self.__hours = hours
+        if not(0 <= minutes <= 59):
+            minutes = 0
+        self.__minutes = minutes
