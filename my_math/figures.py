60942987eaa7235abcf6a0cd050a5fd88defe481
FFFOOOOOOLEGENDA <plotnikovma08@gmail.com>
2024-02-24 18:08:32 +0300

    Исправил недочёт с пробелами

--- my_math/figures.py
+++ my_math/figures.py
@@ -10,22 +10,22 @@ class Square:
 
 
 class Rectangle:
-	def __init__(self, side_a, side_b):
-		self.a = side_a
-		self.b = side_b
+    def __init__(self, side_a, side_b):
+        self.a = side_a
+        self.b = side_b
 
-	def sey_square(self):
-		return self.a * self.b
+    def sey_square(self):
+        return self.a * self.b
 		
-	def sey_perimeter(self):
-		return (self.a + self.b) * 2
+    def sey_perimeter(self):
+        return (self.a + self.b) * 2
 
 class Circle:
-	def __init__(self, radius):
-		self.radius = radius
+    def __init__(self, radius):
+        self.radius = radius
 
-	def sey_square(self):
-		return 3.14 * self.radius ** 2
+    def sey_square(self):
+        return 3.14 * self.radius ** 2
 
-	def sey_perimeter(self):
-		return 2 * 3.14 * self.radius
+    def sey_perimeter(self):
+        return 2 * 3.14 * self.radius
