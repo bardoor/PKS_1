13fecbb5eed1e4f98b3ebce734400dfc4b60debd
Demazsik <kostyaevdema@gmail.com>
2024-02-28 16:13:20 +0300

    функции квадрата
    создал функции для нахождения перемитра и площади квадрата

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 class Square:
      def init(self, side_square:float):
         self.side_square = side_square
+        
+    def Square_perimeter(self):
+        return(self.side_square * 4)
+
+    def Square_square(self):
+        return(self.side_square * 2)
