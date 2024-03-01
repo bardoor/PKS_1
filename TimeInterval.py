ae98501241fe7e02d83d43c3249a37eb4fd1f92e
shadowflexer <ikhanin2008@mail.ru>
2024-03-01 21:09:34 +0300

    Сделал приватным поле seconds(нынешний __seconds) у класса TimeInterval
    Метод теперь является приватным, также во всех методах, что использовали это поле: seconds теперь заменено на актуальное __seconds

--- TimeInterval.py
+++ TimeInterval.py
@@ -1,54 +1,54 @@
 class TimeInterval:
-    seconds: int
+    __seconds: int
 
     def __init__(self, seconds: int=0, minutes: int=0, hours: int=0) -> None:
         if not(seconds >= 0 and minutes >= 0 and hours >= 0):
             raise RuntimeError("Warning! Hours, minutes, seconds must be greater than or equal to 0")
-        self.seconds = seconds + minutes * 60 + hours * 3600
+        self.__seconds = seconds + minutes * 60 + hours * 3600
 
     def __add__(self, other: 'TimeInterval') -> 'TimeInterval':
-        return TimeInterval(self.seconds + other.seconds)
+        return TimeInterval(self.__seconds + other.__seconds)
 
     def __sub__(self, other: 'TimeInterval') -> 'TimeInterval':
-        if self.seconds < other.seconds:
+        if self.__seconds < other.__seconds:
             raise RuntimeError("Warning! Time interval cannot be less than 0")
-        return TimeInterval(self.seconds - other.seconds)
+        return TimeInterval(self.__seconds - other.__seconds)
     
     def __eq__(self, other: 'TimeInterval') -> bool:
-        return self.seconds == other.seconds
+        return self.__seconds == other.__seconds
     
     def __ne__(self, other: 'TimeInterval') -> bool:
-        return self.seconds != other.seconds
+        return self.__seconds != other.__seconds
     
     def __lt__(self, other: 'TimeInterval') -> bool:
-        return self.seconds < other.seconds
+        return self.__seconds < other.__seconds
 
     def __gt__(self, other: 'TimeInterval') -> bool:
-        return self.seconds > other.seconds
+        return self.__seconds > other.__seconds
     
     def __le__(self, other: 'TimeInterval') -> bool:
-        return self.seconds <= other.seconds
+        return self.__seconds <= other.__seconds
 
     def __ge__(self, other: 'TimeInterval') -> bool:
-        return self.seconds >= other.seconds
+        return self.__seconds >= other.__seconds
     
     def get_time_interval_in_nanoseconds(self) -> int:
-        return self.seconds * 1000000000
+        return self.__seconds * 1000000000
     
     def get_time_interval_in_seconds(self) -> int:
-        return self.seconds
+        return self.__seconds
     
     def get_time_interval_in_minutes(self) -> float:
-        return self.seconds / 60
+        return self.__seconds / 60
     
     def get_time_interval_in_hours(self) -> float:
-        return self.seconds / 3600
+        return self.__seconds / 3600
     
     def get_time_interval_in_days(self) -> float:
-        return self.seconds / 86400
+        return self.__seconds / 86400
     
     def __str__(self) -> str:
-        hours = self.seconds // 3600
-        minutes = (self.seconds - hours * 3600) // 60
-        seconds = self.seconds - hours * 3600 - minutes * 60
+        hours = self.__seconds // 3600
+        minutes = (self.__seconds - hours * 3600) // 60
+        seconds = self.__seconds - hours * 3600 - minutes * 60
         return f"В данном промежутке времени есть {hours} часов, {minutes} минут, {seconds} секунд"
