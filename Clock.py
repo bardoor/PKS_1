39a82ad600f1705a82c0f5d9a469cdba81bf4501
Tumani1337 <idi_suda06@mail.ru>
2024-03-05 21:34:24 +0300

    исправил ошибки в коде

--- Clock.py
+++ Clock.py
@@ -3,10 +3,10 @@ class Clock:
     __minutes: int
 
     def __init__(self, __hour: int = 0, __minutes: int = 0):
-        if not(0 <= __hour >= 23)
+        if not(0 <= __hour >= 23):
             raise RunTimeError('invalid value, the number of hours must fall in the range from 0 to 23 inclusive')
         self.__hour = __hour
-        if not(0 <= __minutes >= 59)
+        if not(0 <= __minutes >= 59):
             raise RunTimeError('invalid value, the number of minutes must fall in the range from 0 to 59 inclusive')
         self.__minutes = __minutes
 
@@ -26,4 +26,5 @@ class Clock:
     def get_minutes(self):
         return self.__minutes
 
-    
+    def __str__(self):
+        return f'{self.__hour}:{self.__minutes}'
