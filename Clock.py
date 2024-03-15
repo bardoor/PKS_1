dfc583632e327185b1f3bfa59ebf41c45525c4a2
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-03-15 14:41:45 +0300

    Добавлен Конструктор Класса

--- Clock.py
+++ Clock.py
@@ -1,5 +1,14 @@
 class Clock:
     __minutes: int
     __hours: int
+    def __init__(self, minutes: int, hours: int):
+        if not (0 <= hours <= 23):
+            raise RuntimeError("Ошибка. Неправильный ввод Часов!")
+        self.__hours = hours
+        
+        if not (0 <= minutes <= 59):
+            raise RuntimeError("Ошибка. Неправильный ввод Минут!")
+        self.__minutes = minutes
 
+    
     
