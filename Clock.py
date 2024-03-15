03b81a11808db092f7e646cd512baabf09f4d216
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-03-15 14:55:44 +0300

    Добавлен Метод, установление минут

--- Clock.py
+++ Clock.py
@@ -1,14 +1,22 @@
 class Clock:
     __minutes: int
     __hours: int
+
     def __init__(self, minutes: int, hours: int):
-        if not (0 <= hours <= 23):
+        if not (0 <= hours <= 23.9):
             raise RuntimeError("Ошибка. Неправильный ввод Часов!")
         self.__hours = hours
         
-        if not (0 <= minutes <= 59):
+        if not (0 <= minutes <= 59.9):
             raise RuntimeError("Ошибка. Неправильный ввод Минут!")
         self.__minutes = minutes
 
-    
-    
+    def set_hours(self, hours: int):
+        if not (0 <= hours <= 23.9):
+            raise RuntimeError("Ошибка. Неправильный ввод Часов!")
+        self.__hours = hours
+
+    def set_minutes(self, minutes: int):
+        if not (0 <= minutes <= 59.9):
+            raise RuntimeError("Ошибка. Неправильный ввод Минут!")
+        self.__minutes = minutes
