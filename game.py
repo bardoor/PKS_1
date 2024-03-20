c0069bb3b4147cf4df23c926e0ca03f9522e6f1a
shadowflexer <ikhanin2008@mail.ru>
2024-03-20 14:07:49 +0300

    Создал методы hit и take_damage для класса Creature
    Оба метода ничего не возвращают; Метод take_damage - принимает 2 параметра: self, damage: int. Если damage < 0 - метод выбрасывает исключение - raise RuntimeError с надписью, что урон не может быть отрицательным, если же damage >= 0, то происходит: self._hp -= damage; Метод hit - принимает 2 параметра: self, enemy: 'Creature'. Единственное что делает метод - enemy.take_damage(self._strength)

--- game.py
+++ game.py
@@ -11,3 +11,11 @@ class Creature(ABC):
         self._name = name
         self._hp = hp
         self._strength = strength
+
+    def hit(self, enemy: 'Creature') -> None:
+        enemy.take_damage(self._strength)
+
+    def take_damage(self, damage: int) -> None:
+        if damage < 0:
+            raise RuntimeError("Урон не может быть отрицательным")
+        self._hp -= damage
