4f73391057e2f5a8d02373503d45072e02ab4005
shadowflexer <ikhanin2008@mail.ru>
2024-03-20 14:00:28 +0300

    Создал абстрактный класс Creature, поля и метод __init__ для него
    Класс Creature - дочерний класс класса ABC, у Creature есть поля: _name: str; _hp: int; _strength: int. Также у класса есть метод __init__ который принимает 4 параметра: self, name: str, hp: int, strength: int. Метод __init__ ничего не возвращает, а только устанавливает значения, а именно - self._name = name; self._hp = hp; self._strength = strength

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
 from abc import ABC
 import random
+
+
+class Creature(ABC):
+    _name: str
+    _hp: int
+    _strength: int
+
+    def __init__(self, name: str, hp: int, strength: int) -> None:
+        self._name = name
+        self._hp = hp
+        self._strength = strength
