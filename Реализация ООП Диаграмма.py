baa19133a99899eff856ca707fecdabcc799f6b0
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-04-02 23:24:16 +0300

    Добавлен Класс User и его Методы

---
+++ Реализация ООП Диаграмма.py
@@ -0,0 +1,9 @@
+class User:
+    __name: str
+    def __init__(self, name: str):
+        self.__name = name
+    def start_computer(self):
+        pass
+    def turn_computer(self):
+        pass
+
