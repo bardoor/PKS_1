d748cb629c36a4f6b1bd0ac565e6cbf84ffaa0b9
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-04-03 00:05:33 +0300

    Добавлен Класс Wheel и его Методы

--- Plays.py
+++ Plays.py
@@ -15,3 +15,25 @@ class Direction(Enum):
     North = 2
     West = 3
     East = 4
+
+class Wheel:
+    __center_x: int
+    __center_y: int
+    __radius: int
+    __color: int
+
+    def __init__(self, center_x, center_y, radius: int, color: (int, int, int)):
+        self.__center_x = center_x
+        self.__center_y = center_y
+        self.__radius = radius
+        self.__color = color
+
+    def draw(self, surface: pygame.Surface):
+        pygame.draw.circle(surface=surface,
+                           color=self.__color,
+                           center=(self.__center_x, self.__center_y),
+                           radius=self.__radius)
+
+    def move(self, offset_x: int, offset_y: int):
+        self.__center_x += offset_x
+        self.__center_y += offset_y
