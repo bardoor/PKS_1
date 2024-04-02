57f61f62eacc0c8c70f2a041cd6a6983ec66ac8c
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-04-03 00:04:38 +0300

    Добавление класс direction

--- Plays.py
+++ Plays.py
@@ -8,3 +8,10 @@ WINDOW_WIDTH = 500
 BACKGROUND_COLOR = (255, 255, 255)
 CAPTION = "Моя прекрасная вагонетка-самоходка"
 FPS = 60
+
+
+class Direction(Enum):
+    South = 1
+    North = 2
+    West = 3
+    East = 4
