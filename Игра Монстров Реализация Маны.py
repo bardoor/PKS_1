d65646fc6b7659a5ea902aba6a6974217469ceae
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-04-02 23:49:19 +0300

    Добавлен Класс Creature и его Методы

--- Игра Монстров Реализация Маны.py
+++ Игра Монстров Реализация Маны.py
@@ -1,3 +1,31 @@
 from abc import ABC
 import random
 import os
+
+class Creature(ABC):
+    def __init__(self, name: str, hp: int, strength: int, mana: int):
+        self._hp = hp
+        self._strength = strength
+        self._name = name
+        self._mana = mana
+
+    def hit(self, enemy: 'Creature'):
+        enemy.take_damage(self._strength)
+
+    def take_damage(self, damage: int):
+        self._hp -= damage
+
+    def get_hp(self):
+        return self._hp
+
+    def get_mana(self):
+        return self._mana
+
+    def get_name(self):
+        return self._name
+
+    def is_alive(self):
+        return self._hp > 0
+
+    def __str__(self):
+        pass
