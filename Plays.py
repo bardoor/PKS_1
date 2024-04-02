34a50b0012a44cb94186ce5b090b10053eb24714
Dienly-Bogdan <156441373+Dienly-Bogdan@users.noreply.github.com>
2024-04-03 00:03:30 +0300

    Добавление всех библиотек

---
+++ Plays.py
@@ -0,0 +1,10 @@
+import pygame
+from enum import Enum
+
+pygame.init()
+
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 500
+BACKGROUND_COLOR = (255, 255, 255)
+CAPTION = "Моя прекрасная вагонетка-самоходка"
+FPS = 60
