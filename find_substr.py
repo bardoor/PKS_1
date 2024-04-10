e8a76dfb69341395f858aa8992e6de608e2ead4f
shadowflexer <ikhanin2008@mail.ru>
2024-04-10 10:30:51 +0300

    Создал функции: test_first_str_is_empty_in_find_substr и test_second_str_is_empty_in_find_substr для функции find_substr, которые проверяют ее на случай передачи в нее пустой/ых строк
    В случае если в find_substr передать пустую/ые строку/и, функция должна вернуть -1, что после добавления проверки они и будет делать

--- find_substr.py
+++ find_substr.py
@@ -1,6 +1,8 @@
 def find_substr(big_str: str, little_str: str) -> None | int:
     if not(isinstance(big_str, str) and isinstance(little_str, str)):
         return None
+    if big_str == '' or little_str == '':
+        return -1
     if not(little_str in big_str):
         return None
     for big_str_symbol_index in range(len(big_str)):
@@ -34,3 +36,15 @@ def test_second_str_in_first_str_twice_or_more_in_find_substr() -> None:
     second_param = '12'
     must_return = 4
     assert find_substr(first_param, second_param) == must_return, "В первой строке несколько вторых строк"
+
+def test_first_str_is_empty_in_find_substr() -> None:
+    first_param = ''
+    second_param = 'fafa'
+    must_return = -1
+    assert find_substr(first_param, second_param) == must_return, "Первая строка является пустой"
+
+def test_second_str_is_empty_in_find_substr() -> None:
+    first_param = 'vjkhvba'
+    second_param = ''
+    must_return = -1
+    assert find_substr(first_param, second_param) == must_return, "Вторая строка является пустой"
