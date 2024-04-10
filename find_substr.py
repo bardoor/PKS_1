ee825b4dfd062cb2f347451e5050ad63772a2d7f
shadowflexer <ikhanin2008@mail.ru>
2024-04-10 10:25:47 +0300

    Создал функцию test_not_second_str_in_first_str_in_find_substr, которая проверяет функцию find_substr, на случай если переданная в ней вторая строка не будет находиться в первой
    Функция передает функции find_substr 2 строки, при чем первая не содержит вторую, также изменил саму find_substr под этот случай

--- find_substr.py
+++ find_substr.py
@@ -1,6 +1,8 @@
 def find_substr(big_str: str, little_str: str) -> None | int:
     if not(isinstance(big_str, str) and isinstance(little_str, str)):
         return None
+    if not(little_str in big_str):
+        return None
     for big_str_symbol_index in range(len(big_str)):
         if big_str[big_str_symbol_index] == little_str[0]:
             we_got_correct_index = True
@@ -20,3 +22,9 @@ def test_not_str_in_params_in_find_substr() -> None:
     second_param = 12
     must_return = None
     assert find_substr(first_param, second_param) == must_return, "В параметрах переданы не строки"
+
+def test_not_second_str_in_first_str_in_find_substr() -> None:
+    first_param = 'fav21'
+    second_param = '0'
+    must_return = None
+    assert find_substr(first_param, second_param) == must_return, "В первой строке нету второй"
